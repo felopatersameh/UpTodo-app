8a45c9b596ae013a5fe4455e1d6dea74520b57f7
Felopater Sameh <felopaters37@gmail.com>
2024-09-22 16:54:29 +0300

    create some of screens

---
+++ lib/features/Focus/presentation/pages/focus_screen.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/material.dart';class FocusScreen extends StatelessWidget {  const FocusScreen({super.key});  @override  Widget build(BuildContext context) {    return Scaffold(      body: SafeArea(        child: Container(),      ),    );  }}
---
+++ lib/features/Setting/presentation/pages/setting_screen.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/material.dart';class SettingScreen extends StatelessWidget {  const SettingScreen({super.key});  @override  Widget build(BuildContext context) {    return Scaffold(      body: SafeArea(        child: Container(),      ),    );  }}
