f6566b829b32957233e1d4aabbe0bda8c4054182
Felopater Sameh <felopaters37@gmail.com>
2025-07-03 12:13:40 +0300

    update name

--- lib/core/utils/resource/constant.dart
+++ lib/core/utils/resource/constant.dart
@@ -1,1 +1,10 @@
-import 'package:flutter/material.dart';class AppConstant {  static const String kImage =      "https://media.licdn.com/dms/image/v2/D4D03AQEsBirk013W0g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1731256654216?e=1736985600&v=beta&t=ZRWJa18QZeELdG6NeUZrzat78l9rHY9OzJXh195i5ic";  static ScrollPhysics physics = RangeMaintainingScrollPhysics();  static const String kName = "Felopater Sameh";  static const String kPass = "123456789#";  static const String tagSearch = "search";}
+import 'package:flutter/material.dart';
+
+class AppConstant {
+  static const String kImage =
+      "https://media.licdn.com/dms/image/v2/D4D03AQEsBirk013W0g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1731256654216?e=1736985600&v=beta&t=ZRWJa18QZeELdG6NeUZrzat78l9rHY9OzJXh195i5ic";
+  static ScrollPhysics physics = RangeMaintainingScrollPhysics();
+  static const String kName = "Guest";
+  static const String kPass = "123456789#";
+  static const String tagSearch = "search";
+}
